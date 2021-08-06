0bb1c86b18536e1d1040f94e7c5bd504da0d961f
Matej Petras <matej.petras@tholyn.sk>
2021-08-06 13:30:39 +0200

    ConstAdder IO handling and execute

---
+++ core/src/main/java/sk/catheaven/model/cpu/components/ConstAdder.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.catheaven.model.cpu.components;
+
+import sk.catheaven.model.Data;
+import sk.catheaven.model.cpu.Component;
+import sk.catheaven.service.IOHandler;
+
+/**
+ * Single Input -> Single Output component
+ * Adder, which adds a constant to the input. The constant is fixed after first 
+ * loaded.
+ * @author catlord
+ */
+public class ConstAdder extends Component {
+    private Data constant = new Data(CPU.getBitSize() / Byte.SIZE); // default size of constant
+    
+    @Override
+    public Data getInput(String inputLabel) {
+        return IOHandler.getSingleInput(getInputs());
+    }
+    
+    @Override
+    public Data getOutput(String outputLabel) {
+        return IOHandler.getSingleOutput(getOutputs());
+    }
+    
+    @Override
+    public void execute() {
+        this.getOutput(Component.IGNORED_LABEL).setData(
+            this.getInput(Component.IGNORED_LABEL).getData() + constant.getData()
+        );
+    }
+    
+    public Data getConstant() {
+        return constant;
+    }
+    
+    public void setConstant(Data constant) {
+        this.constant = constant;
+    }
+}
