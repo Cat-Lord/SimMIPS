2bacd99ab09a186d90ac0f75b3fd0c38dfa8d21a
Matej Petras <matej.petras@tholyn.sk>
2021-08-06 13:38:09 +0200

    Fork IO handling and execute

--- core/src/main/java/sk/catheaven/model/cpu/components/Fork.java
+++ core/src/main/java/sk/catheaven/model/cpu/components/Fork.java
@@ -3,9 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sk.catheaven.model.components;
+package sk.catheaven.model.cpu.components;
+
+import sk.catheaven.model.Data;
+import sk.catheaven.model.cpu.Component;
+import sk.catheaven.service.IOHandler;
 
 /**
+ * Single Input -> Single Output component
  * Forks the input value into exactly multiple output values. It is expected
  * to fork a single input value into two output values, but not required/constrained.
  * Input json file is expected to define output array of values. If this array 
@@ -17,5 +22,23 @@ package sk.catheaven.model.components;
  * @author catlord
  */
 public class Fork extends Component {
-
+    @Override
+    public Data getInput(String inputLabel) {
+        return IOHandler.getSingleInput(getInputs());
+    }
+    
+    @Override
+    public Data getOutput(String outputLabel) {
+        return IOHandler.getSingleOutput(getOutputs());
+    }
+    
+    @Override
+    public void execute() {
+        for (Data output : getOutputs().values()) {
+            // every output knows how to handle the input because
+            // they have specific range which they apply to input
+            // see the method:   Data.getData()
+            output.setData(this.getInput(Component.IGNORED_LABEL));
+        }
+    }
 }
