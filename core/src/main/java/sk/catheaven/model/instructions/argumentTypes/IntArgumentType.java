26d45b0c78b2cc12ba54e6bdead346f1ad8785cd
Matej Petras <matej.petras@tholyn.sk>
2021-08-06 19:37:24 +0200

    IntArgumentType implemented

--- core/src/main/java/sk/catheaven/model/instructions/argumentTypes/IntArgumentType.java
+++ core/src/main/java/sk/catheaven/model/instructions/argumentTypes/IntArgumentType.java
@@ -5,7 +5,33 @@
  */
 package sk.catheaven.model.instructions.argumentTypes;
 
+import sk.catheaven.model.Data;
 import sk.catheaven.model.instructions.ArgumentType;
 
 public class IntArgumentType extends ArgumentType {
+    private final Data integer;
+    
+    public IntArgumentType() {
+        integer = new Data(16);				// maximal amount of bits per int number // todo - why ?
+    }
+    
+    @Override
+    public boolean isValidArgument(String argument) {
+        try {
+            argument = argument.trim();
+            integer.setData(Integer.parseInt(argument));
+        } catch(NumberFormatException e){
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
+    public Data getData(String argument) {
+        if (isValidArgument(argument))
+            return integer;
+        return null;
+    }
+    
+    
 }
