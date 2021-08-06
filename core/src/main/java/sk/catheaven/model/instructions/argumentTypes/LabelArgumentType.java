74a16c5e917235069d7567c7754ebc3c5f9fc9a7
Matej Petras <matej.petras@tholyn.sk>
2021-08-06 19:40:38 +0200

    LabelArgumentType implemented

--- core/src/main/java/sk/catheaven/model/instructions/argumentTypes/LabelArgumentType.java
+++ core/src/main/java/sk/catheaven/model/instructions/argumentTypes/LabelArgumentType.java
@@ -5,7 +5,34 @@
  */
 package sk.catheaven.model.instructions.argumentTypes;
 
+import sk.catheaven.model.Data;
 import sk.catheaven.model.instructions.ArgumentType;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Represent label argument. Label can be almost anything, format is as follows:
+ *  Any ASCII character first, can be followed by any number of characters and numbers.
+ * Valid characters are _0123456789a-zA-Z
+ * @author catlord
+ */
 public class LabelArgumentType extends ArgumentType {
+    public LabelArgumentType() {
+        pattern = Pattern.compile(LABEL_REGEX);
+    }
+    
+    /**
+     * Parses argument to detect possible label malformation.
+     * @param argument Represent instruction argument
+     */
+    @Override
+    public boolean isValidArgument(String argument) {
+        return pattern.matcher(argument).matches();
+    }
+    
+    @Override
+    public Data getData(String argument) {
+        return null;
+    }
 }
