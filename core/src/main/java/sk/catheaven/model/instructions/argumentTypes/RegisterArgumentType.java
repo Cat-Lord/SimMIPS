925e83cc6d7a988627a4e53bc953a560b2d52038
Matej Petras <matej.petras@tholyn.sk>
2021-08-06 19:45:14 +0200

    RegisterArgumentType implemented

---
+++ core/src/main/java/sk/catheaven/model/instructions/argumentTypes/RegisterArgumentType.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.catheaven.model.instructions.argumentTypes;
+
+import sk.catheaven.model.Data;
+import sk.catheaven.model.instructions.ArgumentType;
+import sk.catheaven.service.Assembler;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Represents register argument type. A register begins with letter R (lower or upper case)
+ * and an index number. Example: r4, R124, r14, etc.
+ * Index limitation depends on the size of register bank of CPU, but allows index overflows
+ * (Indexes are stored in Data objects and are properly cut if needed).
+ * @author catlord
+ */
+public class RegisterArgumentType extends ArgumentType {
+    private final Data registerIndex;
+    
+    public RegisterArgumentType(){
+        registerIndex = new Data(5);
+        pattern = Pattern.compile(REGISTER_REGEX);  // todo - again, why this hard-coded value ?
+    }
+    
+    @Override
+    public boolean isValidArgument(String argument) {
+        return pattern.matcher(argument).matches();
+    }
+    
+    /**
+     * From valid argument extracts number and returns it as integer value.
+     * @param argument String representation of register, for example "r3" or "R72".
+     * @return register index
+     */
+    @Override
+    public Data getData(String argument){
+        argument = argument.trim().toLowerCase();
+        String regIndexString = argument.substring(
+                                    argument.indexOf(Assembler.REGISTER_SYMBOL) + 1,
+                                    argument.length()
+                                );
+        registerIndex.setData(Integer.parseInt(regIndexString));
+        return registerIndex;
+        
+    }
+}
