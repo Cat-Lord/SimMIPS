d681d20b5e51bafa70bf549f2895471682b28963
Matej Petras <matej.petras@tholyn.sk>
2021-08-09 17:42:59 +0200

    ControlUnit Test + json adjustment

---
+++ core/src/test/java/sk/catheaven/cpu/components/ControlUnitTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.catheaven.cpu.components;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import sk.catheaven.cpu.CPUContainer;
+import sk.catheaven.model.Data;
+import sk.catheaven.model.cpu.Component;
+import sk.catheaven.model.cpu.components.ControlUnit;
+import sk.catheaven.utils.DataFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ *
+ * @author catlord
+ */
+// TODO - this needs to get bigger -> testing for different opCodes and aluOP values as well as signal extraction
+public class ControlUnitTest extends CPUContainer {
+	private static ControlUnit cu;
+	
+	@BeforeAll
+	static void prepareComponent() {
+		cu = (ControlUnit) cpu.getComponents().get("CU");
+	}
+	
+	@Test
+	public void test(){
+		Data data = new Data(); // initiate empty data
+		cu.setInput(Component.IGNORED_LABEL, data);
+		cu.execute();
+		
+		assertEquals("100 0000 0011", DataFormatter.getBinary(cu.getOutput(Component.IGNORED_LABEL)));
+	}
+	
+}
