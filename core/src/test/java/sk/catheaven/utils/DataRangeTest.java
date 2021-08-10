2e5c01468c6adfebaa9d350f201eb3b7e449b198
Matej Petras <matej.petras@tholyn.sk>
2021-08-10 14:03:49 +0200

    Data Range Test (testing if data correctly extract parts of input using range like i.e. "10-25")

---
+++ core/src/test/java/sk/catheaven/utils/DataRangeTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.catheaven.utils;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import sk.catheaven.model.Data;
+import sk.catheaven.model.Tuple;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ *
+ * @author catlord
+ */
+@DisplayName("Data should be able to")
+public class DataRangeTest {
+	private static Data data;
+	@BeforeAll
+	static void prepareTestingData() {
+		data = new Data();
+	}
+	
+	@Test
+	@DisplayName("extract data from input with shifting by amount specified in range")
+	public void testShortRange(){
+		data.setData(69);
+		
+		Tuple<String, String> shortRange = new Tuple<>("21", "25");
+		Data shortRangeCutter = new Data(data.getBitSize());
+		shortRangeCutter.setRange(shortRange);
+		assertEquals(data.getBitSize(), shortRangeCutter.getBitSize());
+		
+		data.setData(11121745);
+		shortRangeCutter.setData(data);
+		assertEquals(69, shortRangeCutter.getData());
+		
+		data.setData(176836986);
+		Tuple<String, String> wideRange = new Tuple<>("8", "25");
+		Data wideRangeCutter = new Data(data.getBitSize());
+		wideRangeCutter.setRange(wideRange);
+		assertEquals(data.getBitSize(), wideRangeCutter.getBitSize());
+		
+		wideRangeCutter.setData(data);
+		assertEquals(69, wideRangeCutter.getData());
+	}
+}
