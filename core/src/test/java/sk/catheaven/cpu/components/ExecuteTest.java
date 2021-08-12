fd7edc54161c4615917da42b782aca0d3fb168bd
Matej Petras <matej.petras@tholyn.sk>
2021-08-12 15:37:49 +0200

    CPU execute cycle test

--- core/src/test/java/sk/catheaven/cpu/components/ExecuteTest.java
+++ core/src/test/java/sk/catheaven/cpu/components/ExecuteTest.java
@@ -1,23 +1,40 @@
 package sk.catheaven.cpu.components;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sk.catheaven.cpu.CPUContainer;
 import sk.catheaven.model.cpu.Executable;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+@DisplayName("CPU should")
 public class ExecuteTest extends CPUContainer {
     
     @Test
-    public void executeTest() {
+    @DisplayName("correctly call execute on every of its components")
+    public void componentSingleExecuteTest() {
         for (Executable component : cpu.getComponents().values())
             assertDoesNotThrow(component::execute);
     }
     
     @Test
-    public void multipleExecutes() {
+    @DisplayName("correctly call execute multiple times on every of its components")
+    public void componentMultipleExecuteCallsTest() {
         for (int i = 0; i < 15; i++)
             for (Executable component : cpu.getComponents().values())
                 assertDoesNotThrow(component::execute);
+    }
+    
+    @Test
+    @DisplayName("correctly perform cycle execution")
+    public void executeSingleCycle() {
+        cpu.executeCycle();
+    }
+    
+    @Test
+    @DisplayName("correctly perform cycle execution multiple times in a row")
+    public void executionCycleMultipleTimes() {
+        for (int i = 0; i < 15; i++)
+            cpu.executeCycle();
     }
 }
