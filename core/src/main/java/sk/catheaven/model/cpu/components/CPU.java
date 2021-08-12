af36b6334914fe092a11522a14f2b15be45f9cec
Matej Petras <matej.petras@tholyn.sk>
2021-08-12 15:06:49 +0200

    CPU is now loading connectors

--- core/src/main/java/sk/catheaven/model/cpu/components/CPU.java
+++ core/src/main/java/sk/catheaven/model/cpu/components/CPU.java
@@ -17,7 +17,7 @@ public class CPU {
     
     public static int BIT_SIZE = 32;
     private Map<String, Component> components = new LinkedHashMap<>();
-    private List<Connector> connectors = new ArrayList<>();
+    private Map<String, List<Connector>> connectors;
     private List<List<Component>> phases;
     
     public CPU() {
@@ -43,11 +43,11 @@ public class CPU {
         CPU.BIT_SIZE = bitSize;
     }
     
-    public List<Connector> getConnectors() {
+    public Map<String, List<Connector>> getConnectors() {
         return connectors;
     }
     
-    public void setConnectors(List<Connector> connectors) {
+    public void setConnectors(Map<String, List<Connector>> connectors) {
         if (areValidConnectors(connectors))
             this.connectors = connectors;
     }
@@ -56,32 +56,6 @@ public class CPU {
         return phases;
     }
     
-    private boolean areValidConnectors(List<Connector> connectors) {
-        boolean ret = true;
-        for (Connector connector : connectors) {
-            
-            // check if component 'from' exists
-            if (components.get(connector.getFrom()) == null) {
-                log.error("Connector invalid: Component FROM `{}` missing", connector.getFrom());
-                ret = false;
-            }
-            
-            // check if component 'from' exists
-            if (components.get(connector.getTo()) == null) {
-                log.error("Connector invalid: Component TO `{}` missing", connector.getFrom());
-                ret = false;
-            }
-            
-            // check if selectors exist
-            if (components.get(connector.getFrom()).getOutputs().get(connector.getSelector()) == null) {
-                log.error("Component FROM `{}` doesn't provide specified OUTPUT `{}`",
-                        components.get(connector.getFrom()).getLabel(),
-                        connector.getSelector());
-                ret = false;
-            }
-        }
-        return ret;
-    }
     
     /**
      * Calculates number of bytes for this CPU's architecture. Example:
@@ -114,5 +88,58 @@ public class CPU {
         phasesList.add(currentPhase);
 
         return phasesList;
+    }
+    
+    private boolean areValidConnectors(Map<String, List<Connector>> connectors) {
+        boolean ret = true;
+        
+        for (String sourceComponentLabel : connectors.keySet()) {
+            Component sourceComponent = components.get(sourceComponentLabel);
+            
+            // check if component 'from' exists
+            if (sourceComponent == null) {
+                log.error("Connector invalid: Component FROM `{}` missing", sourceComponentLabel);
+                ret = false;
+                continue;
+            }
+
+            // check if component 'to' exists
+            for (Connector connector : connectors.get(sourceComponentLabel)) {
+                
+                // check if source is multi-output and has the specified output labeled this way
+                if (sourceComponent.isSingleOutputComponent() == false  &&
+                        sourceComponent.getOutput(connector.getSelector()) == null) {
+                    
+                    log.error("Connector invalid: Source component `{}` doesn't provide output `{}`",
+                            sourceComponentLabel,
+                            connector.getSelector()
+                    );
+                    ret = false;
+                    continue;
+                }
+
+                Component targetComponent = components.get(connector.getTo());
+                
+                if (targetComponent == null) {
+                    log.error("Connector invalid: Component from {} TO `{}` missing",
+                                    sourceComponentLabel,
+                                    connector.getTo()
+                    );
+                    ret = false;
+                    continue;
+                }
+                
+                // check if selectors in target inputs exist
+                if (targetComponent.isSingleInputComponent() == false &&
+                        targetComponent.getInput(connector.getSelector()) == null) {
+                    log.error("Connector invalid: From {} | Target component `{}` doesn't provide specified INPUT `{}`",
+                            sourceComponentLabel,
+                            components.get(connector.getTo()).getLabel(),
+                            connector.getSelector());
+                    ret = false;
+                }
+            }
+        }
+        return ret;
     }
 }
